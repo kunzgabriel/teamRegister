c6dd9debff5c62177ac4a720357bec873baab438
kunzgabriel <gabrielkunz2012@gmail.com>
2021-01-04 17:53:06 -0300

    +featude
    adicionada funcao para limpar a lista de jogadores da equipe

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const playersList = document.getElementById("teamPlayers")
 const addPlayerButton = document.getElementById("addPlayerButton")
 const playerName = document.getElementById("playerName")
 const playerRole = document.getElementById("playerRole")
+const removePlayersButton = document.getElementById("removePlayersButton")
 
 const addPlayer = () =>{
     if(playerName.value != ''){
         let nome = playerName.value
         let role = playerRole.value.trim()
-        console.log('Adicionado o player', nome)
+        console.log('Adicionado o player', nome, '!!')
         const player = document.createElement('li')
         if(role == ''){
             player.innerHTML = `<li>${nome}</li>`
@@ -24,4 +25,10 @@ const addPlayer = () =>{
     }
 }
 
+const removePlayers = () =>{
+    playersList.innerHTML = '';
+    console.log('A lista de jogadores foi limpa!!')
+}
+
 addPlayerButton.addEventListener('click', addPlayer)
+removePlayersButton.addEventListener('click', removePlayers)
